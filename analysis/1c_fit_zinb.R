b72aca1289d008304596317f73b3f30c62fb0d38
julaluehring <98964094+julaluehring@users.noreply.github.com>
2025-08-20 05:38:48 -0600

    Update 1c_fit_zinb.R

--- analysis/1c_fit_zinb.R
+++ analysis/1c_fit_zinb.R
@@ -17,7 +17,7 @@ dv <- args[1]
 src <- args[2]
 dst <- "./zinb/"
 df <- read_csv(file.path(src, 
-                "matched_starters.csv"),
+                "matched_starters_mahalanobis.csv"),
                 col_types = cols()) %>%
       suppressMessages()
 
